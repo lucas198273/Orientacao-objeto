e4740a86330530f58a9fc44b9731cfba6f5bc9d7
Lucas Dias <lucasinga2000@gmail,com>
2023-04-15 09:31:43 -0300

    ultimo commit

--- orientação Objeto/src/calculos/calculo2/calculator.java
+++ orientação Objeto/src/calculos/calculo2/calculator.java
@@ -1,10 +1,7 @@
 package calculos.calculo2;
 
 public class calculator {
-  //  if(static in metodo ){
-  //      Basta chamar a classe pelo nome
-  //       e selecionar o metodo desejao
-  //  }else Devera instaciar a o objeto no programa principal
+	
     public static final double PI = 3.14;
 
     public static double circuference(double raidus){
